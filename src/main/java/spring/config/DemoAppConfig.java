f3d887102b89ad1e2c2e3f05ce0fffb43c9b783a
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2020-01-24 11:38:35 +0000

    Added but not tested jdbc authentication with plaintext passwords

--- src/main/java/spring/config/DemoAppConfig.java
+++ src/main/java/spring/config/DemoAppConfig.java
@@ -1,18 +1,70 @@
 package spring.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="spring")
+@PropertySource("classpath:persistence.properties")
 public class DemoAppConfig {
 
-    // define a bean for ViewResolver
+    @Autowired
+    private Environment env; // Hold data read from properties file
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Bean
+    public DataSource securityDataSource() {
+
+        // create connection pool
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        // set the jdbc driver
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        }
+        catch (PropertyVetoException exc) {
+            throw new RuntimeException(exc);
+        }
+
+        // for sanity sake to log the url and user just to make sure we are reading the data
+        logger.info(">>>> jdbc.url=" + env.getProperty("jdbc.url"));
+        logger.info(">>>> jdbc.user=" + env.getProperty("jdbc.user"));
+
+        // set database connection properties
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        // set the connection pool properties
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    private int getIntProperty(String propName) {
+        String propVal = env.getProperty(propName);
+
+        int intPropVal = Integer.parseInt(propVal);
+
+        return intPropVal;
+    }
 
     @Bean
     public ViewResolver viewResolver() {
--- src/main/java/spring/config/DemoSecurityConfig.java
+++ src/main/java/spring/config/DemoSecurityConfig.java
@@ -1,5 +1,6 @@
 package spring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,19 +8,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-                .withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-                .withUser(users.username("susan").password("test123").roles("EMPLOYEE" ,"ADMIN"));
+        auth.jdbcAuthentication().dataSource(securityDataSource);
 
     }
 
